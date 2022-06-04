c0fa19946db6066dbf8f235f28e039df014a7f75
Evangelos Lioudakis <elioudakis123@gmail.com>
2022-06-04 17:47:54 +0300

    Debugging

--- main.py
+++ main.py
@@ -5,72 +5,77 @@ from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+from ulauncher.api.shared.action.ExtensionCustomAction import OpenUrlAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 
 from github import Github
 
+import threading
+
 logger = logging.getLogger(__name__)
 
-url = "https://book.hacktricks.xyz/welcome/readme"
+hacktricks_url = "https://book.hacktricks.xyz/welcome/readme"
+github_url = "https://github.com/carlospolop/hacktricks."
+
+blacklisted_files = [".git", ".gitbook", ".github", ".gitignore"]
+
+# make the search_files function run in a separate thread
+
+
+def search_files(repo_code, repo, q, root):    
+    results = []
+    results_file = open("results.txt", "w")
+    for file in repo_code:
+        if file.name not in blacklisted_files:
+            new_filename = root + "/" + file.name
+            print(f"checking {new_filename} for {q}")
+            if type(repo.get_contents(new_filename)) == list:
+                search_files(repo.get_contents(new_filename), repo, q, new_filename)
+            else:
+                if q in repo.get_contents(new_filename).decoded_content.decode("utf-8"):
+                    print(f"{new_filename} contains {q}")
+                    results_file.write(f"{new_filename}\n")
+                else:
+                    continue
+
+def query_github(g, q):
+    repo = g.get_repo("carlospolop/hacktricks")
+    root = ""
+    repo_code = repo.get_contents(root)
+    search_files(repo_code, repo, q, root)
+
+
+def query_hacktricks(q):
+    url = f"{url}?q={q}"
+    
+def open_url(url):
+    pass
 
 class SearchHacktricks(Extension):
 
     def __init__(self):
         super(SearchHacktricks, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
-        
-    def query_github(g, q):
-        rate_limit = g.get_rate_limit()
-        rate = rate_limit.search
-        if rate.remaining == 0:
-            print(f'You have 0/{rate.limit} API calls remaining. Reset time: {rate.reset}')
-            return
-        else:
-            print(f'You have {rate.remaining}/{rate.limit} API calls remaining')
-    
-        query = f'"{keyword} english" in:file extension:md'
-        result = g.search_code(query, order='desc')
-    
-        max_size = 15
-        print(f'Found {result.totalCount} file(s)')
-        if result.totalCount > max_size:
-            result = result[:max_size]
-    
-        for file in result:
-            print(f'{file.download_url}')
-
-
-    def query_hacktricks(q):
-        url = f"{url}?q={q}"
         
 
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
         g = Github(extension.preferences['github_token'])
-        items = []
+        x = threading.Thread(target=query_github, args=(g,q,), daemon=True)
+        results = open("results.txt", "r")
         logger.info('preferences %s' % json.dumps(extension.preferences))
-        for i in range(5):
-            item_name = "iteam"
-            data = {'new_name': '%s %s was clicked' % (item_name, i)}
+        for result in results.readlines():
+            url = result.url
+            result_name = result.split('/')[-1]
+            # data = {'name': '%s %s was clicked' % (item_name, i)}
             items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='%s %s' % (item_name, i),
-                                             description='Item description %s' % i,
-                                             on_enter=ExtensionCustomAction(data, keep_app_open=True)))
+                                             name='%s %s' % (result_name),
+                                             description=result_name,
+                                             on_enter=OpenUrlAction(url)))
 
         return RenderResultListAction(items)
-
-
-class ItemEnterEventListener(EventListener):
-
-    def on_event(self, event, extension):
-        data = event.get_data()
-        return RenderResultListAction([ExtensionResultItem(icon='images/icon.png',
-                                                           name=data['new_name'],
-                                                           on_enter=HideWindowAction())])
 
 
 if __name__ == '__main__':
